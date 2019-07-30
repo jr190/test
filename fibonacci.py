9fadf29125e2ff59ae114e4a9016f1811bb2fc02
jr <ramirezjmrjmr@gmail.com>
2019-07-30 17:33:38 -0400

    added fibonacci sequence function

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,17 @@
-print('Test')
+user_input = int(input('How many Fibonacci numbers would you like to generate? '))
+
+
+def fib_seq(repeats):
+    first_num = 1
+    second_num = 1
+    print(first_num)
+    print(second_num)
+    third_num = first_num + second_num
+    for x in range(repeats):
+        first_num = second_num
+        second_num = third_num
+        third_num = first_num + second_num
+        print(third_num)
+    return
+
+fib_seq(user_input)
