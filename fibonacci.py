1d0fcb44506fa00abe81f5e4d665af245c194d1f
jr <ramirezjmrjmr@gmail.com>
2019-07-30 18:58:39 -0400

    final push

--- fibonacci.py
+++ fibonacci.py
@@ -5,14 +5,24 @@ def fib_seq(repeats):
     '''Generates the Fibonacci Sequence'''
     first_num = 1
     second_num = 1
-    print(first_num)
-    print(second_num)
-    third_num = first_num + second_num
-    for x in range(repeats):
-        first_num = second_num
-        second_num = third_num
+    if repeats <= 0:
+        return
+    elif repeats == 1:
+        print(first_num)
+        return
+    elif repeats == 2:
+        print(first_num)
+        print(second_num)
+        return
+    else:
+        print(first_num)
+        print(second_num)
         third_num = first_num + second_num
-        print(third_num)
+        for x in range(repeats):
+            first_num = second_num
+            second_num = third_num
+            third_num = first_num + second_num
+            print(third_num)
     return
 
 fib_seq(user_input)
